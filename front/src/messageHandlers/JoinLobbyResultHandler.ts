2e16ea1ae66de5257cba8acb09d6e8134da3f7bc
kjbowman00 <kj.bowman00@gmail.com>
2024-08-26 16:24:39 -0400

    Temp

--- front/src/messageHandlers/JoinLobbyResultHandler.ts
+++ front/src/messageHandlers/JoinLobbyResultHandler.ts
@@ -1,12 +1,23 @@
 import { JoinLobbyResult, JoinLobbyResultRef } from "../../../state/src/sockets/serverMessageTypes/JoinLobbyResult";
 import { isValid } from "../../../state/src/sockets/Validator";
+import { MenuManager } from "../components/MenuManager";
 
 export class JoinLobbyResultHandler {
+    menuManager: MenuManager;
 
-    handle(data: any) {
+    constructor(menuManager:MenuManager) {
+        this.menuManager = menuManager;
+    }
+
+    handle(data: any, ) {
         if ( ! isValid(data, JoinLobbyResultRef) ) return;
 
         const joinLobbyResult = data as JoinLobbyResult;
-        
+        if (! joinLobbyResult.success) return; //TODO: set to main menu maybe?
+
+        // Update menu to show lobby
+        // this.menuManager.lobbyMenu.setLobbyNameDisplay(joinLobbyResult.lobbyID);
+        this.menuManager.lobbyMenu.setPlayersNamesDisplay([]);
+        this.menuManager.switchToLobbyMenu();
     }
 }
